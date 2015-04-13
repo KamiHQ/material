bc121fcfabf4c8401b30ca37e3dd0eacde98f805
Xiliang Chen <xlchen1291@gmail.com>
2015-04-13 15:13:03 +1200

    use NotablePDF/bower-material

--- scripts/bower-material-release.sh
+++ scripts/bower-material-release.sh
@@ -15,7 +15,7 @@ function run {
 
   echo "-- Cloning bower-material..."
   rm -rf bower-material
-  git clone https://angular:$GH_TOKEN@github.com/angular/bower-material \
+  git clone git@github.com:NotablePDF/bower-material.git \
     bower-material --depth=2
 
   echo "-- Copying in build files..."
@@ -35,7 +35,7 @@ function run {
   git push -q origin master
   git push -q origin v$VERSION
 
-  echo "-- Version $VERSION pushed successfully to angular/bower-material!"
+  echo "-- Version $VERSION pushed successfully to NotablePDF/bower-material!"
 
   cd ../
 }
